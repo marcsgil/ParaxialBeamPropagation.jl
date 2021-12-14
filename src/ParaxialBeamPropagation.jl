e30e0359f555aade29445a3fc92cf5b49a03cd5c
Marcos Gil <marcosgildeoliveira@gmail.com>
2021-12-14 11:53:02 -0300

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.23

---
+++ src/ParaxialBeamPropagation.jl
@@ -0,0 +1,5 @@
+module ParaxialBeamPropagation
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ParaxialBeamPropagation
+using Test
+
+@testset "ParaxialBeamPropagation.jl" begin
+    # Write your tests here.
+end
